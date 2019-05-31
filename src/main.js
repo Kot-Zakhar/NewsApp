733797f7bdf1d1e81886dc1dc3d8747f3d4f0300
ZakharKot <kot_zakhar@mail.ru>
2019-06-01 00:43:48 +0300

    Bootstraped a tamplate

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import Application from './src/js/app';
+
+const App = new Application();
+App.start();
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    entry: './main.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'babel-loader'
+            }]
+        }]
+    },
+    stats: {
+        colors: true
+    }
+};
